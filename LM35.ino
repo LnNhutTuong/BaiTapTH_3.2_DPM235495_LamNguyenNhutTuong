493bd5be2bc1562363201b489c661217ff19641a
tuong <you@example.com>
2026-08-19 15:09:44 +0700

    bai tap 2

---
+++ LM35.ino
@@ -0,0 +1,31 @@
+/*
+ * Created by ArduinoGetStarted.com
+ *
+ * This example code is in the public domain
+ *
+ * Tutorial page: https://arduinogetstarted.com/tutorials/arduino-lm35-temperature-sensor
+ */
+
+
+int sensorPin = A0;
+int ledPin = 13;
+int sensorValue = 0;
+
+void setup() {
+
+  
+pinMode(ledPin, OUTPUT);
+
+}
+
+void loop() {
+  sensorValue  = analogRead (sensorPin);
+
+  digitalWrite(ledPin, HIGH);
+
+  delay(sensorValue);
+
+  digitalWrite(ledPin, LOW);
+  
+  delay(sensorValue);
+}
